9953eb46dfff3d7ae57bc415961f7b50b3b67875
Justice Peltier <peltierjustice@gmail.com>
2020-10-09 19:36:05 -0400

    Added urls, views, and template

--- django-project/projects/views.py
+++ django-project/projects/views.py
@@ -1,6 +1,10 @@
 from django.http import request
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
-def project_list(request):
-    return render(request, 'projects/index.html')
+def all_projects(request):
+    # query database to return all project objects
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html',
+                    {'projects': projects})

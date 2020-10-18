c6a308295bbdbb0380b7ea4989d84d1de0e60faa
Justice Peltier <peltierjustice@gmail.com>
2020-10-18 12:30:15 -0400

    NoReverseMatch Resolution
    app_name was not set

--- django-project/blog/urls.py
+++ django-project/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
     path("<int:pk>/", views.blog_detail, name="blog_detail"),

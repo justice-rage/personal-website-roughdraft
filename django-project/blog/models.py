5899a7f23545f121eef3a618517d035c8f29c3de
Justice Peltier <peltierjustice@gmail.com>
2020-10-17 21:19:54 -0400

    Deleted .html files, added models improvement
    1) templates aren't rendering. Error: TemplateDoesNotExist
    
    2) Models addition returns name/title of object instead of 'object (#)'

--- django-project/blog/models.py
+++ django-project/blog/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=20)
+    
+    # returns name/title of object instead of 'object (#)'
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -10,6 +14,10 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+
+    # returns name/title of object instead of 'object (#)'
+    def __str__(self):
+        return self.title #or self.name
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)

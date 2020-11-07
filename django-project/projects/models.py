694651e9fad0c02a27d3bf756259c3a7346d48cc
Justice Peltier <peltierjustice@gmail.com>
2020-11-07 17:19:33 -0500

    Modified models: larger text field allowance

--- django-project/projects/models.py
+++ django-project/projects/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
+    technology = models.CharField(max_length=100)
     image = models.CharField(max_length=100)
     
     # returns name/title of object instead of 'object (#)'

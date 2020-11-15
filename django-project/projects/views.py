de7223a100b10cfce89244ada02759a7284fc0a6
Justice Peltier <peltierjustice@gmail.com>
2020-11-15 12:10:12 -0500

    Streamlined and renamed templates
    Templates streamlined/placed into project level template folder

--- django-project/projects/views.py
+++ django-project/projects/views.py
@@ -5,10 +5,10 @@ from projects.models import Project
 def all_projects(request):
     # query database to return all project objects
     projects = Project.objects.all()
-    return render(request, 'projects/all_projects.html',
+    return render(request, 'projects/projects_index.html',
                     {'projects': projects})
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
-    return render(request, 'projects/detail.html',
+    return render(request, 'projects/projects_detail.html',
                     {'project': project})

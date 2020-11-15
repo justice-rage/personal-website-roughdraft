4628daeb3fd1f323015a92660d5d78aa76e38873
Justice Peltier <peltierjustice@gmail.com>
2020-11-15 09:54:05 -0500

    Created root static folder
    Created root static folder, and added the following files: js, css, img

--- django-project/portfolio/settings.py
+++ django-project/portfolio/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Developer added: Static settings
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

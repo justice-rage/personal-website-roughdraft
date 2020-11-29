5dd352dc8841c0acff1aab7cdbe5c24a78414494
Justice Douglas <justice.douglas@hey.com>
2020-11-29 13:12:30 -0500

    Removed unnecessary note

--- django-project/portfolio/urls.py
+++ django-project/portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
-    path('blog/', include('blog.urls')), # may require ' , '
+    path('blog/', include('blog.urls')),
 ]

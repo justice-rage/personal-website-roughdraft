9d22cd412ab8a99fa13d07daf0f6baa0d1412e5c
Justice Douglas <justice.douglas@hey.com>
2020-12-03 14:58:30 -0500

    Added technology function to models
    Projects can now be looked up according to technology utilized, like categories.

--- django-project/projects/admin.py
+++ django-project/projects/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from projects.models import Project
+from projects.models import Project, Technology
 
 # Register your models here.
+
+class TechnologyAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Project)
+admin.site.register(Technology, TechnologyAdmin)

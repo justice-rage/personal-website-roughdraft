aee7d30e74ab400c4e440dae2a8ecde62c680e2d
Justice Douglas <justice.douglas@hey.com>
2020-12-06 15:24:55 -0500

    Migrated SQLite to PostgreSQL

--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -79,10 +79,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+# Original SQLite Database
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+DATABASES ={
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'justice_website',
+        'USER': 'postgres',
+        'PASSWORD': 'SilenceGrows1!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
